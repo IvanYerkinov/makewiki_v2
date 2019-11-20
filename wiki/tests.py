4908cb07b02653c68dc42f1962834ac05d4e4fa2
Ivan Yerkinov <darkblackvoid@yahoo.com>
2019-11-20 15:18:07 -0800

    Finished tests

--- wiki/tests.py
+++ wiki/tests.py
@@ -8,8 +8,23 @@ class WikiTestCase(TestCase):
     def test_true(self):
         self.assertEqual(True, True)
 
-    def test_creation_page(self):
+    def test_get(self):
         response = self.client.get('/new/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_creation_page(self):
+
+        user = User.objects.create()
+
+        dict = {'title': "TEST", 'author': 1, 'content': 'test'}
+
+        response = self.client.post('/new/', dict)
+
+        self.assertEqual(response.status_code, 200)
+
+        slug = Page.objects.get(title="TEST").slug
+
+        response = self.client.get('/' + slug + '/')
 
         self.assertEqual(response.status_code, 200)
 
