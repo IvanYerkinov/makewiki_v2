7e90ed6b73afd638afdd9e6a187280c97f770c16
Ivan Yerkinov <darkblackvoid@yahoo.com>
2019-11-20 14:54:05 -0800

    Added tests

--- wiki/tests.py
+++ wiki/tests.py
@@ -1,7 +1,23 @@
 from django.test import TestCase
+from wiki.models import Page
+from django.contrib.auth.models import User
 
 
 # Create your tests here.
 class WikiTestCase(TestCase):
     def test_true(self):
         self.assertEqual(True, True)
+
+    def test_creation_page(self):
+        response = self.client.get('/new/')
+
+        self.assertEqual(response.status_code, 200)
+
+    def test_slug_page(self):
+        user = User.objects.create()
+        Page.objects.create(title="TEST CONTENT", content="test", author=user)
+        slug = Page.objects.get(title="TEST CONTENT").slug
+
+        response = self.client.get('/' + slug + '/')
+
+        self.assertEqual(response.status_code, 200)
